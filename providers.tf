f6706c8e58395c7a403f0c7c56b4463a17254e52
Vinícius Massari <stonefields11@hotmail.com>
2025-01-29 10:39:06 -0300

    remote state config

--- providers.tf
+++ providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "5.72.1"
     }
   }
+
+  backend "s3" {
+    state = "rocketseat-state-bucket-tf"
+    region = "us-east-2"
+    key = "terraform.tfstate"
+    encrypt = true
+  }
+  
 }
 
 provider "aws" {
@@ -12,3 +20,22 @@ provider "aws" {
 }
 
 
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [aws_s3_bucket.terraform_state]
+
+}
